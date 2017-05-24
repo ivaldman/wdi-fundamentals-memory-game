f530b91702e3e1397d5c75f50875ef88b2af618e
Ivy Valdman <ivyvaldman@Ivys-Air.attlocal.net>
2017-05-23 20:25:13 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,17 +1,26 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log ("User flipped queen.");
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log ("User flipped king.");
-if ( cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You have a match!");
+
+var checkForMatch = function() {
+	 if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("You found a match!");
+  } else {
+      console.log("Sorry, try again.");
+  }
 }
-else if (cardsInPlay[0] !== cardsInPlay[2]) {
-    alert("Sorry try again!");
-}
+
+var flipCard = function(cardId) {
+    console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+    checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
+
+
+
+
 
 
 
